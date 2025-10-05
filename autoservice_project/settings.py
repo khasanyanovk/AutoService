2b201f12bce3e77eba535c074523131e0c3a3797
khasanyanovk <hasanyanovk2@mail.ru>
2025-10-06 00:41:13 +0300

    some changes in profile

--- autoservice_project/settings.py
+++ autoservice_project/settings.py
@@ -118,9 +118,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-STATICFILES_DIRS = [BASE_DIR / "core/static"]
+STATICFILES_DIRS = [BASE_DIR / "static", BASE_DIR / "core" / "static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
@@ -128,7 +128,7 @@ STATICFILES_DIRS = [BASE_DIR / "core/static"]
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
-MEDIA_URL = "media/"
+MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
