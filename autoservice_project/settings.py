9e438bf279164d453b64135d27c6e606ce1fa407
khasanyanovk <hasanyanovk2@mail.ru>
2025-11-17 15:40:36 +0300

    fix media_root

--- autoservice_project/settings.py
+++ autoservice_project/settings.py
@@ -164,7 +164,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
 MEDIA_URL = "/media/"
-MEDIA_ROOT = [BASE_DIR / "media"]
+MEDIA_ROOT = BASE_DIR / "media"
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 CRISPY_TEMPLATE_PACK = "bootstrap5"
