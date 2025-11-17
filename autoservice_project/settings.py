9fe8faf18b677f1eee797c6135d7d7abaf730672
khasanyanovk <hasanyanovk2@mail.ru>
2025-11-17 14:38:42 +0300

    migrate data to postgres

--- autoservice_project/settings.py
+++ autoservice_project/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 import os
-import environ
+from environ import Env  # type: ignore
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-env = environ.Env(
+env = Env(
     DEBUG=(bool, False),
     SECRET_KEY=(str, "insecure-dev-secret-key-change-me"),
-    ALLOWED_HOSTS=(list, ['localhost', '127.0.0.1']),
+    ALLOWED_HOSTS=(list, ["localhost", "127.0.0.1"]),
     DJANGO_TIME_ZONE=(str, "Europe/Moscow"),
     DB_ENGINE=(str, "django.db.backends.sqlite3"),
     DB_NAME=(str, str(BASE_DIR / "db.sqlite3")),
@@ -33,7 +33,7 @@ env = environ.Env(
     DB_HOST=(str, ""),
     DB_PORT=(str, ""),
     EMAIL_BACKEND=(str, "django.core.mail.backends.smtp.EmailBackend"),
-    EMAIL_HOST=(str,"smtp.gmail.com"),
+    EMAIL_HOST=(str, "smtp.gmail.com"),
     EMAIL_PORT=(int, 587),
     EMAIL_HOST_USER=(str, ""),
     EMAIL_HOST_PASSWORD=(str, ""),
@@ -47,7 +47,7 @@ env = environ.Env(
     LOG_LEVEL=(str, "ERROR"),
 )
 
-environ.Env.read_env(BASE_DIR / ".env")
+Env.read_env(BASE_DIR / ".env")
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # Read from environment; provide a clearly insecure default for local dev only
@@ -109,8 +109,12 @@ WSGI_APPLICATION = "autoservice_project.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": env("DB_ENGINE"),
+        "NAME": env("DB_NAME"),
+        "USER": env("DB_USER"),
+        "PASSWORD": env("DB_PASSWORD"),
+        "HOST": env("DB_HOST"),
+        "PORT": env("DB_PORT"),
     }
 }
 
@@ -171,18 +175,18 @@ CRISPY_TEMPLATE_PACK = "bootstrap5"
 EMAIL_BACKEND = env("EMAIL_BACKEND")
 
 EMAIL_HOST = env("EMAIL_HOST")
-EMAIL_PORT = ("EMAIL_PORT")
+EMAIL_PORT = "EMAIL_PORT"
 EMAIL_HOST_USER = env("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
 
 EMAIL_USE_TLS = env("EMAIL_USE_TLS")
 EMAIL_USE_SSL = env("EMAIL_USE_SSL")
 
-EMAIL_TIMEOUT = ("EMAIL_TIMEOUT")
+EMAIL_TIMEOUT = "EMAIL_TIMEOUT"
 
-DEFAULT_FROM_EMAIL=env("DEFAULT_FROM_EMAIL")
-SERVER_EMAIL=env("SERVER_EMAIL")
-REPLY_TO_EMAIL=env("REPLY_TO_EMAIL")
+DEFAULT_FROM_EMAIL = env("DEFAULT_FROM_EMAIL")
+SERVER_EMAIL = env("SERVER_EMAIL")
+REPLY_TO_EMAIL = env("REPLY_TO_EMAIL")
 
 # Optional: predefined list of admin emails to notify (comma-separated)
 NOTIFY_ADMINS_EMAILS = env("NOTIFY_ADMINS_EMAILS")
